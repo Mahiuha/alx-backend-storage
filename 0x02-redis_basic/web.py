06fc4eece9c791ff7371e51eb39e5e906b7f322b
Mahiuha <tiasho.mahiuha@gmail.com>
2022-04-27 23:23:19 +0300

    Implemented an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,46 +1,28 @@
 #!/usr/bin/env python3
-"""Get a page and count times accessed"""
-
-
-import requests
-from functools import wraps
+''' Implement expiring web cache and tracker '''
 import redis
-from typing import Union, Callable
+import requests
+from typing import Callable
+from functools import wraps
+
+r = redis.Redis()
 
 
-red = redis.Redis()
-red.flushdb()
-
-
-def my_cache(method: Callable) -> Callable:
-    """G e t  a  p a g e  a n d  c o u n t  t i m e s  a c c e s s e d"""
+def count(method: Callable) -> Callable:
+    ''' Count method '''
     @wraps(method)
-    def wrap(*args, **kwargs):
-        """G e t  a  p a g e  a n d  c o u n t  t i m e s  a c c e s s e d"""
-        url = args[0]
-        text = red.get(url)
-        if text is None:
-            text = method(*args, **kwargs)
-            if text is not None:
-                red.setex(url, 10, text)
-                red.incr("count:"+url)
-        else:
-            text = text
-            red.incr("count:"+url)
-        return text
-    return wrap
+    def wrapper(*args, **kwargs):
+        ''' wrapper '''
+        r.incr('count:' + args[0])
+        p = r.get(args[0])
+        if not p:
+            p = method(*args, **kwargs)
+            r.setex(args[0], 10, p)
+        return p
+    return wrapper
 
 
-@my_cache
-def get_url(url: str) -> bytes:
-    """G e t  a  p a g e  a n d  c o u n t  t i m e s  a c c e s s e d"""
-    try:
-        text = requests.get(url).content
-        return text
-    except Exception:
-        return None
-
-
+@count
 def get_page(url: str) -> str:
-    """G e t  a  p a g e  a n d  c o u n t  t i m e s  a c c e s s e d"""
-    return get_url(url)
+    ''' Get page '''
+    return requests.get(url).text
