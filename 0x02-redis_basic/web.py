aa765ecc8583401d29ffac329915104d88f89e98
Mahiuha <tiasho.mahiuha@gmail.com>
2022-04-27 23:35:08 +0300

    Implemented an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,35 +2,39 @@
 """
 Web file
 """
-import requests
+
 import redis
+import requests
+from typing import Callable
 from functools import wraps
 
-store = redis.Redis()
+rd = redis.Redis()
 
 
-def count_url_access(method):
-    """ Decorator counting how many times
-    a Url is accessed """
+def count_requests(method: Callable) -> Callable:
+    """ Counting with decorators how many times a request
+        has been made
+    """
+
     @wraps(method)
     def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
+        """ Wrapper for decorator functionality """
+        rd.incr(f"count:{url}")
+        cached_html = rd.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
 
-        count_key = "count:" + url
         html = method(url)
-
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
+        rd.setex(f"cached:{url}", 10, html)
         return html
+
     return wrapper
 
 
-@count_url_access
+@count_requests
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+    """ requests module to obtain the HTML
+        content of a particular URL and returns it.
+    """
+    req = requests.get(url)
+    return req.text
